6812af9596ed983979a9d921f29a104a4f0bf31b
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-10 22:00:17 +0900

    move_to_index 除外対象追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     before_action :set_prototype, only: [:edit, :show]
-    before_action :move_to_index, except: [:index, :show]
+    before_action :move_to_index, except: [:index, :show, :new, :create, :update, :destroy]
     def index
         @prototype = Prototype.all
     end
