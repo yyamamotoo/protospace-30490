ec2feaf584a146e253fdf092f1b9da2249defdd3
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-10 21:06:18 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,68 @@
+class PrototypesController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_prototype, only: [:edit, :show]
+    before_action :move_to_index, except: [:index, :show]
+    def index
+        @prototype = Prototype.all
+    end
+
+    def new
+        @prototype = Prototype.new
+    end
+
+    def create
+         @prototype = Prototype.new(prototype_params)
+        if @prototype.save
+          redirect_to root_path
+        else
+            @prototype = Prototype.new(prototype_params)
+          render :new
+        end
+    end
+
+    def show
+        @comment = Comment.new
+        @comments = @prototype.comments.includes(:user)
+    end
+
+    def edit
+    end
+
+    def update
+        prototype = Prototype.find(params[:id])
+        prototype.update(prototype_params)
+        prototype.save
+        if prototype.save
+          redirect_to root_path
+        else
+          @prototype = Prototype.find(params[:id])
+          render :edit
+        end
+
+    end
+
+    def destroy
+        prototype = Prototype.find(params[:id])
+        prototype.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def prototype_params
+        params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    end
+
+    def set_prototype
+        @prototype = Prototype.find(params[:id])
+    end
+
+    def move_to_index
+        
+        unless @prototype.user == current_user
+          redirect_to action: :index
+        end
+      end
+
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        @prototype = @user.prototypes
+      end
+
+end
